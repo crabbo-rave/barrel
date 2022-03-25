9852b47354ef3da9c1ca8244cfb62438f768d451
crab <92041779+crabbo-rave@users.noreply.github.com>
2022-03-25 08:04:42 -0400

    Update std.rkt

--- barrel/std.rkt
+++ barrel/std.rkt
@@ -4,12 +4,12 @@
 
 (define (newline str) (string-append-immutable str "\n"))
 
-(define (+!-helper op acc stack)
+(define (sum-helper op acc stack)
     (if (equal? stack empty)
         acc
         (block
           (define a (first stack))
-          (+!-helper op (op a acc) (rest stack)))))
+          (sum-helper op (op a acc) (rest stack)))))
 
 ; (define (^ stack) (display (newline (~v (first stack)))))
 (define ($ stack) (first stack))
@@ -18,12 +18,12 @@
 (define / empty)
 (define λ null)
 (define (~ stack) (cons null stack))
-(define (roll stack) (reverse stack))
+(define (& stack) (reverse stack))
 (define (: stack) (cons (first stack) stack))
 (define (+! stack)
-  (cons (+!-helper + 0) stack))
+  (cons (sum-helper + 0 stack) stack))
 (define (*! stack)
-  (cons (+!-helper * 0) stack))
+  (cons (sum-helper * 0 stack) stack))
 
 (define (handle-args . args)
   (for/fold ([stack-acc empty])
@@ -33,7 +33,7 @@
       [(? string? arg) (cons arg stack-acc)]
       [(== /)
        arg]
-      [(or (== %) (== !) #| (== ^) |# (== $) (== roll) (== :) (== ~) (== *!) (== +!))
+      [(or (== %) (== !) #| (== ^) |# (== $) (== &) (== :) (== ~) (== *!) (== +!))
        (arg stack-acc)]
       [(or (== +) (== *))
        (define op-result (arg (first stack-acc) (second stack-acc)))
@@ -42,4 +42,4 @@
        (raise "revenge of the lambda" #t)])))
 (provide handle-args)
 
-(provide + * #| ^ |# $ % ! +! λ / roll : *! ~)
+(provide + * #| ^ |# $ % ! +! λ / & : *! ~)
